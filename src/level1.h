7ab5bb0be481152daf2777bc2d0160de4c9c5c9f
Anha Khan <anhakhan0111@gmail.com>
2026-07-05 13:56:14 +0600

    Level 1 complete: background, pits, countdown, moving obstacles

--- src/level1.h
+++ src/level1.h
@@ -8,8 +8,15 @@
 #include "obstacle.h"
 #include <stdbool.h>
 
+typedef struct Pit
+{
+    float x;
+    float width;
+} Pit;
+
 typedef enum Level1State
 {
+    L1_COUNTDOWN,
     L1_PLAYING,
     L1_WIN,
     L1_LOSE
@@ -22,9 +29,12 @@ typedef struct Level1
     float timeLeft;
     int coinsCollected;
     Level1State state;
+    float countdownTimer;
     Coin coins[LEVEL1_COIN_COUNT];
     Obstacle obstacles[LEVEL1_OBS_COUNT];
+    Pit pits[LEVEL1_PIT_COUNT];
     Texture2D coinTexture;
+    Texture2D bgFar;
 } Level1;
 
 Level1 Level1Create(void);
